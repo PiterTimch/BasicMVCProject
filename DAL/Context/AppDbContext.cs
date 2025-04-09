be4f8cdc62306fa29b6751fc60ecb468bd64fcda
Petro <petrotymchuck@gmail.com>
2025-04-09 16:47:36 +0300

    Add basic DAL configuration

---
+++ DAL/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities.Category;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<CategoryEntity> Categories { get; set; };
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ DAL/Entities/Category/CategoryEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Entities.Category
+{
+    public class CategoryEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Url]
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
