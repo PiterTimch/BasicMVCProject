ec75041c88433e1f41ce03cb1e0bf5566f680123
Petro <petrotymchuck@gmail.com>
2025-04-16 14:50:59 +0300

    add cropper (only design)

---
+++ BasicMVCProject/wwwroot/js/imageCropper.js
@@ -0,0 +1,39 @@
+﻿let input, image, croppedDataInput, cropper, form, cropContainer;
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM fully loaded and parsed");
+    input = document.getElementById("ImageFile");
+    image = document.getElementById("croppingImage");
+    croppedDataInput = document.getElementById("croppedImageData");
+    form = document.querySelector("form");
+    cropContainer = document.getElementById("imageCropContainer");
+
+    input.addEventListener("change", function () {
+        const file = this.files[0];
+        if (file && /^image\/\w+/.test(file.type)) {
+            const url = URL.createObjectURL(file);
+            image.src = url;
+            cropContainer.style.display = "block";
+            image.style.display = "block";
+
+            if (cropper) cropper.destroy();
+
+            cropper = new Cropper(image, {
+                aspectRatio: 16 / 9,
+                viewMode: 1,
+                autoCropArea: 1,
+                cropend: function () {
+                    const canvas = cropper.getCroppedCanvas();
+                    croppedDataInput.value = canvas.toDataURL("image/webp");
+                }
+            });
+        }
+    });
+
+    form.addEventListener("submit", function () {
+        if (cropper) {
+            const canvas = cropper.getCroppedCanvas();
+            croppedDataInput.value = canvas.toDataURL("image/webp");
+        }
+    });
+});
